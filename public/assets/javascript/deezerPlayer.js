6cd027f4ca00fcedc07048fa13204434d4736724
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-11-26 13:28:02 +0100

    read button work

--- public/assets/javascript/deezerPlayer.js
+++ public/assets/javascript/deezerPlayer.js
@@ -1,17 +1,12 @@
 var playerButton = document.getElementById('player-button');
 var id = document.getElementById('id');
 var player = document.getElementById('player');
-var displayed = false;
+
 
 playerButton.onclick = function () {
-    if (displayed) {
-        var myIframe = document.getElementById('my-iframe');
-        myIframe.remove();
-        displayed = false;
-    } else {
         var iframe = "<iframe id='my-iframe' scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\" src=\"http://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=" + id.value + "&app_id=1\" width=\"300\" height=\"300\"></iframe>"
         player.innerHTML = iframe;
-        displayed = true;
+        playerButton.disabled = true;
 
         const departMinutes = document.getElementById("timer").innerHTML;
         let temps = departMinutes * 60;
@@ -28,10 +23,10 @@ playerButton.onclick = function () {
             timerElement.innerText = `${minutes}:${secondes}`;
             temps = temps <= 0 ? 0 : temps - 1;
         }, 1000);
-        var myIframe = document.getElementById('my-iframe');
 
-        setTimeout(function(){myIframe.remove();}, (departMinutes*60000)+2000);
-
-
-    }
+        setTimeout(function () {
+            var myIframe = document.getElementById('my-iframe');
+            myIframe.remove();
+            playerButton.disabled = false;
+        }, (departMinutes * 60000) + 2000);
 }
